0845e74e3983e06b5545b5205cabe49871fe6a20
Anna Spjut <anna.spjut@gmail.com>
2024-02-28 00:45:17 +0400

    [fix] input, select & calc totalprice

--- js/script.js
+++ js/script.js
@@ -3,15 +3,7 @@ console.log('esercizio "biglietto treno form"');
 const pricePerKm = 0.21;
 const pricePerKmUnder18 = 0.168;
 const pricePerKmOver65 = 0.126;
-// let totalPrice = '';
 
-
-
-
-//INPUT
-const inputName = document.querySelector('.input-name');
-const inputKm = document.querySelector('.input-km');
-const inputAge = document.querySelector('.input-age');
 
 
 //BUTTONS
@@ -19,15 +11,22 @@ const btnGenerate = document.querySelector('.btn-generate');
 const btnClear = document.querySelector('.btn-clear');
 
 btnGenerate.addEventListener('click',function(){
-  console.log(inputName.value);
-  console.log(inputKm.value);
-  console.log(inputAge.value);
-  if(inputAge < 18){
-    totalPrice = inputKm * pricePerKmUnder18
-  } else if(inputAge > 65){
-    totalPrice = inputKm * pricePerKmOver65
+  const inputName = document.querySelector('.input-name').value;
+  const inputKm = parseInt(document.querySelector('.input-km').value);
+  const age = document.getElementById('age').value;
+
+  console.log(inputName);
+  console.log(inputKm);
+  console.log(age);
+  
+  let totalPrice = inputKm * pricePerKm;
+
+  if(age === 'minorenne'){
+    totalPrice = inputKm * pricePerKmUnder18;
+  } else if(age === 'over65'){
+    totalPrice = inputKm * pricePerKmOver65;
   } else{
-    totalPrice = inputKm * pricePerKm
+    totalPrice = inputKm * pricePerKm;
   }
   console.log(totalPrice.toFixed( 2));
 })
